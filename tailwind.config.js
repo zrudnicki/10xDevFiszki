9f02556b199eedc76633b9bf55928f2aa794be48
Zbigniew Rudnicki <zbyszek.rudnicki@gmail.com>
2025-05-23 22:22:35 +0200

    chore: complete tailwind css configuration

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+      },
+    },
+  },
+  plugins: [],
+} 
